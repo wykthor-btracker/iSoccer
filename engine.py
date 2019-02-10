5fe29bf9a22b63adbdd9fbadf4a8049dffe65075
Wykthor <wykthor.g@wgvale.com>
2019-02-09 23:02:18 -0300

    Será?

--- engine.py
+++ engine.py
@@ -63,6 +63,13 @@ class Validation():
         except:
             raise Exception("{} é um número invalido, formatos aceitos: xxxxx-xxxx ou (x)xxxxxxxx onde x é um inteiro".format(name))
 
+    @staticmethod
+    def validatevalue(name):
+        try:
+            assert(fullmatch(r'R\$\d+\.\d+,\d+', name))
+        except:
+            raise Exception("{} é uma quantitade inválida, formato aceito: R$xx.xx,xx".format(name))
+
 
 class Pessoa():
     def __init__(self, nome, email, telefone, cpf):
@@ -81,15 +88,68 @@ class Pessoa():
         self.instance.index.names = ["CPF"]
 
     def __str__(self):
-        return self.instance
+        return self.instance.__str__()
 
 
 class Funcionario(Pessoa):
     def __init__(self, nome, email, telefone, cpf, salario, **kwargs):
         super().__init__(nome, email, telefone, cpf)
         self.instance["Salário"] = salario
+        cargosvalidos = ["Presidente", "Médico", "Técnico", "Preparador", "Motorista", "Cozinheiro", "Advogado", "Jogador"]
+        if("cargo" in kwargs):
+            if kwargs["cargo"] not in cargosvalidos:
+                raise Exception("{} é um cargo inválido".format(kwargs["cargo"]))
+
         for key in kwargs:
             self.instance[key] = kwargs[key]
+
+
+class Socio(Pessoa):
+    def __init__(self, nome, email, telefone, cpf, endereco, valor, tipo, situ):
+        super().__init__(nome, email, telefone, cpf)
+        self.instance["Contribuição"] = valor
+        self.instance["Classe"] = tipo
+        self.instance["Situação"] = situ
+        self.instance["Endereço"] = endereco
+
+
+class Recurso():
+    def __init__(self, nome, disponivel):
+        self.nome = nome
+        self.disponivel = disponivel
+        self.instance = pd.DataFrame([[disponivel]], columns=["Disponibilidade"], index=[nome])
+        self.instance.index.names = ["Nome"]
+
+    def get(self, campo):
+        if(campo in self.instance.columns):
+            return self.instance[campo]
+        else:
+            return None
+
+    def set(self, campo, valor):
+        if(campo in self.instance.columns):
+            self.instance[campo] = valor
+        else:
+            return False
+        return True
+
+    def __str__(self):
+        return self.instance.__str__()
+
+
+class Estadio(Recurso):
+    def __init__(self, nome, disponivel, capacidade, banheiros, lanchonetes):
+        super().__init__(nome, disponivel)
+        self.instance["capacidade"] = capacidade
+        self.instance["banheiros"] = banheiros
+        self.instance["lanchonetes"] = lanchonetes
+
+
+class Centro(Recurso):
+    def __init__(self, nome, disponivel, dormitorios):
+        super().__init__(nome, disponivel)
+        self.instance["dormitorios"] = dormitorios
+
 # Classes
 
 # Functions
@@ -99,8 +159,8 @@ class Funcionario(Pessoa):
 # Main
 
 
-def main(*args, **kwargs):
-    print(Funcionario("wykthor","a@a.a","9999-9999","00000000000","30",cargo="Prefeito").instance)
+def main():
+    print(Funcionario("wykthor", "a@a.a", "9999-9999", "00000000000", "30", cargo="Técnico"))
     return None
 
 
